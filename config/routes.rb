9ff123e418f7250b0f5166d0c7b5bf16261a3f94
Annie <a.zhangishere@gmail.com>
2018-10-23 02:29:01 -0400

    add migration to users for password-digest, add check in Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-	root 'static_pages#home'
+  root 'static_pages#home'
   get '/help', to:'static_pages#help'
   get '/about', to:'static_pages#about'
   get '/signup', to: 'users#new'
